f30b99af9ecc3a728739338fd089fd800e6745ed
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-06 11:30:25 -0300

    Botando um backgroun e uma fonte muito mais bonitos

--- Crud-React+Cs/src/pages/Home.jsx
+++ Crud-React+Cs/src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
 import './Home.css'
 
 export default function Home() {
-  return (
-    <div className='home-container'>
-        <div>
-            <p>Home</p>
+    return (
+        <div className='home-container'>
+            <video autoplay muted loop playsinline class="video-bg">
+                <source src="./video/background.mp4" type="video/mp4" />
+                Seu navegador não suporta vídeo HTML5.
+            </video>
+
+            <div class="conteudo">
+                <h1>Bem-vindo ao meu site</h1>
+                <p>Texto sobreposto ao vídeo de fundo</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
