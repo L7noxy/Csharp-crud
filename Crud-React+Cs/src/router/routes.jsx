63e3905728f5bc320f87a6d02fc1963ba1a1fbd6
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-06 11:08:00 -0300

    Botando um belo background

--- Crud-React+Cs/src/router/routes.jsx
+++ Crud-React+Cs/src/router/routes.jsx
@@ -1,15 +1,10 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import Tasks from './Tasks'
-import TaskForm from './TaskForm'
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
 
-export default function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tasks" element={<Tasks />} />
-      <Route path="/tasks/new" element={<TaskForm />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  )
-}
+const router = createBrowserRouter([
+
+    { path: "/", element: <Home /> },
+
+])
+
+export default router;
