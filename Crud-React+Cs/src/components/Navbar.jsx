39e4bc839db019acb83d779ebf2698c938970f57
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-09 20:45:45 -0300

    Terminando por hoje

--- Crud-React+Cs/src/components/Navbar.jsx
+++ Crud-React+Cs/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className='container-navbar'>
         <nav className="navbar">
           <div>
-            <img src="./img/LogoCs.png" alt="" srcset="" />
+            <img className="logo-home" src="./img/LogoCs.png" alt="Logo" onClick={() => window.location.href = "https://learn.microsoft.com/en-us/dotnet/csharp/"}/>
           </div>
           <div className="buttons-container">
             <Link to={"/cadastro"}>cadastro</Link>
