1c8a4f40652a2fcf7759ec8f6243332155284c33
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-11 20:33:20 -0300

    Colocando mais um visual na tela de login

--- Crud-React+Cs/src/pages/Login.jsx
+++ Crud-React+Cs/src/pages/Login.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
 import './Login.css';
 
-
 export default function Login() {
   return (
     <div className="login-container">
       <div className="login-left">
-        <img src="./assets/logo.png" alt="Logo da Empresa" className="logo" />
+        <img src='./img/LogoCs.png' alt="Logo da Empresa" className="logo" />
+        <video autoplay muted loop playsinline class="video-bg-cad">
+          <source src="./video/background.mp4" type="video/mp4" />
+          Seu navegador não suporta vídeo HTML5.
+        </video>
       </div>
 
       <div className="login-right">
+
         <h2 className="login-title">Entrar na sua conta</h2>
         <form className="login-form">
           <label htmlFor="email" className="login-label">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="login-input"
-            required
-          />
+          <input type="email" id="email" name="email" className="login-input" required />
 
           <label htmlFor="senha" className="login-label">Senha</label>
-          <input
-            type="password"
-            id="senha"
-            name="senha"
-            className="login-input"
-            required
-          />
+          <input type="password" id="senha" name="senha" className="login-input" required />
 
           <button type="submit" className="login-button">Entrar</button>
         </form>
@@ -38,5 +30,5 @@ export default function Login() {
         </p>
       </div>
     </div>
-    )
+  )
 }
