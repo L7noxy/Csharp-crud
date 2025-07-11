2e24d977ebb133a98471528992e49b35c67b03fa
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-10 21:03:47 -0300

    Fazendo a tela de Login

--- Crud-React+Cs/src/pages/Login.jsx
+++ Crud-React+Cs/src/pages/Login.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login-container">
+      <div className="login-left">
+        <img src="./assets/logo.png" alt="Logo da Empresa" className="logo" />
+      </div>
+
+      <div className="login-right">
+        <h2 className="login-title">Entrar na sua conta</h2>
+        <form className="login-form">
+          <label htmlFor="email" className="login-label">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="login-input"
+            required
+          />
+
+          <label htmlFor="senha" className="login-label">Senha</label>
+          <input
+            type="password"
+            id="senha"
+            name="senha"
+            className="login-input"
+            required
+          />
+
+          <button type="submit" className="login-button">Entrar</button>
+        </form>
+
+        <p className="login-footer">
+          Não tem uma conta? <a href="/cadastro" className="login-link">Cadastre-se</a>
+        </p>
+      </div>
+    </div>
+    )
 }
