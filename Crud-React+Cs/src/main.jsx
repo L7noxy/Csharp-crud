e5856544fa3ba752fe4a6cf0c8a203404112bceb
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-07-10 21:03:08 -0300

    Fazendo a tela de Cadastro

--- Crud-React+Cs/src/main.jsx
+++ Crud-React+Cs/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { GlobalContextProvider } from './context/GlobalContext.jsx'
+import router from './router/routes.jsx'
+import {  RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <GlobalContextProvider>
+      <RouterProvider router={router}>
+      </RouterProvider>
+  </GlobalContextProvider>
 );
- 
--- Crud-React+Cs/src/pages/Cadastro.jsx
+++ Crud-React+Cs/src/pages/Cadastro.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import './Cadastro.css'
 
 export default function Cadastro() {
   return (
-    <div>Cadastro</div>
+    <div className="cadastro-container">
+      <div className="cadastro-left">
+        <img src='./img/LogoCs.png' alt="Logo da Empresa" className="logo" />
+        <video autoplay muted loop playsinline class="video-bg-cad">
+          <source src="./video/background.mp4" type="video/mp4" />
+          Seu navegador não suporta vídeo HTML5.
+        </video>
+      </div>
+
+      <div className="cadastro-right">
+        <h2>Crie sua conta</h2>
+        <form>
+          <label htmlFor="nome">Nome</label>
+          <input type="text" id="nome" name="nome" required />
+
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" name="email" required />
+
+          <label htmlFor="senha">Senha</label>
+          <input type="password" id="senha" name="senha" required />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
   )
 }
